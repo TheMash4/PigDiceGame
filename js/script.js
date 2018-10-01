8267d665b1941971d463dc6fc9d6be02c3841449
mashe <samedumash@gmail.com>
2018-10-01 03:58:39 +0300

    added more styling to the home page

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-function rollDice(){
-    var die1 = document.getElementById("die1");
-    var die2 = document.getElementById("die2");
+function rollDice() {
+// User interface (or front-end) logic:
+    var diceOne = document.getElementById("die1");
+    var diceTwo = document.getElementById("die2");
     var status = document.getElementById("status");
-
-    var d1 = Math.floor(Math.random() * 6)+1;
-    var d2 = Math.floor(Math.random() * 6)+1;
-    var diceTotal = d1+d2;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
-    status.innerHTML = "you rolled"+diceTotal+".";
-    if(d1 == d2){
-        status.innerHTML += "DOUBLES! you get a free turn!!"
+// Business (or back-end) logic:
+    var diceRoll1 = Math.floor(Math.random() * 6) + 1;
+    var diceRoll2 = Math.floor(Math.random() * 6) + 1;
+    var diceTotal = diceRoll1 + diceRoll2;
+    diceOne.innerHTML = diceRoll1;
+    diceTwo.innerHTML = diceRoll2;
+    status.innerHTML = "you rolled" + diceTotal + ".";
+    if (diceRoll1 == diceRoll2) {
+        status.innerHTML += "DOUBLES! Free turn!!"
     }
 
 }
